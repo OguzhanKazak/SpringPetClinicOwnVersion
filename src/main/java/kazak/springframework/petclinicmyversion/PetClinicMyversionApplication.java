8b9fd4b81a25a1194127f0beadef81cb3e48c63c
Oğuzhan Kazak <43974629+OguzhanKazak@users.noreply.github.com>
2020-05-16 14:57:07 +0300

    Project initialization

---
+++ src/main/java/kazak/springframework/petclinicmyversion/PetClinicMyversionApplication.java
@@ -0,0 +1,13 @@
+package kazak.springframework.petclinicmyversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicMyversionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicMyversionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kazak/springframework/petclinicmyversion/PetClinicMyversionApplicationTests.java
@@ -0,0 +1,13 @@
+package kazak.springframework.petclinicmyversion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicMyversionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
